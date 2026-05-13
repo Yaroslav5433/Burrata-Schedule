438290b7017a216cab9eeca9be36a9e5ab47804a
Yaroslav <yaroslav2005pana@gmail.com>
2026-05-13 15:19:12 +0300

    passlib changed with raw bcrypt

--- Burrata/BackEnd/database/models.py
+++ Burrata/BackEnd/database/models.py
@@ -1,18 +1,23 @@
 from sqlalchemy import Column, DateTime, Integer, String, func
-from database import Base
-from passlib.context import CryptContext
-
-password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+from database.database import Base
+from loguru import logger
+import bcrypt
 
 class Admin(Base):
+    __tablename__ = 'admin'
+
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, nullable=False)
+    login = Column(String, unique=True, nullable=False)
     hashed_password = Column(String, nullable=False)
 
     def verify_password(self, password: str) -> bool:
-        return password_context.verify(password, self.hashed_password)
+        return bcrypt.checkpw(
+            password.encode("utf-8"),
+            self.hashed_password.encode("utf-8"))
 
 class Users(Base):
+    __tablename__ = 'users'
+    
     id = Column(Integer, primary_key=True, index=True)
     unique_id_number = Column(String, unique=True, nullable=False)
     username = Column(String, unique=True, nullable=False)
