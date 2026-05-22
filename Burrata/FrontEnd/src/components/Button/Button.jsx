250941eb99898b9f537aad3310427d12621abfbf
Yaroslav <yaroslav2005pana@gmail.com>
2026-05-22 22:18:57 +0300

    Memo added

--- Burrata/FrontEnd/src/components/Button/Button.jsx
+++ Burrata/FrontEnd/src/components/Button/Button.jsx
@@ -1,4 +1,5 @@
 import styles from './button.module.css'
+import { memo } from 'react'
 
 function Button(props) {
     const {
@@ -12,4 +13,4 @@ function Button(props) {
     )
 }
 
-export default Button
+export default memo(Button)
