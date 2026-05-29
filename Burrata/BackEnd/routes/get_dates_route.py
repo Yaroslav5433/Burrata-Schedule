b6b3a045bd7ddf4a10804b53c3f26076d9efea69
Yaroslav <yaroslav2005pana@gmail.com>
2026-05-29 19:26:24 +0300

    separeted get_dates

---
+++ Burrata/BackEnd/routes/get_dates_route.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter
+from loguru import logger
+from schemas.schemas import Dates
+from utils.utils import get_next_week_dates
+
+getdates_router = APIRouter()
+
+@getdates_router.get('/getdates', response_model = Dates)
+def get_users(step: int = 0):
+    dates = get_next_week_dates(step)
+    
+    logger.info('Dates have been calculated')
+    
+    return {'dates': dates}
