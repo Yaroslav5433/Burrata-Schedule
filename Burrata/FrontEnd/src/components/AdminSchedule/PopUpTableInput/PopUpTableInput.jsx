f98bbd1b366f358c3a758923cb1c36e1638231d6
Yaroslav <yaroslav2005pana@gmail.com>
2026-07-26 00:28:12 +0300

    fixed placeholder

--- Burrata/FrontEnd/src/components/AdminSchedule/PopUpTableInput/PopUpTableInput.jsx
+++ Burrata/FrontEnd/src/components/AdminSchedule/PopUpTableInput/PopUpTableInput.jsx
@@ -61,7 +61,7 @@ function PopUpTableInput(props) {
                   label = {showLabel && Object.keys(days)[i]}
                   name = {Object.keys(days)[i]}
                   value = {Object.values(days)[i]}
-                  placeholder={date === 'Saturday' || date === 'Sunday' ? "X/Z/Y": "X/Y"}
+                  placeholder={Object.keys(days)[i] === 'Saturday' || Object.keys(days)[i] === 'Sunday' ? "X/Z/Y": "X/Y"}
                   inputMode="numeric"/>
                 </td>
               ))}
